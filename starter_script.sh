30f07e9b839479142e000ec91df3e2e8a633ee61
omar99 <omarshaikh@outlook.com>
2024-10-01 02:45:44 +0530

    Update starter_script.sh

--- starter_script.sh
+++ starter_script.sh
@@ -3,7 +3,7 @@ systemctl start openvpn@server
 systemctl enable openvpn@server
 
 echo "Authenticating ngrok..."
-ngrok authtoken 2miMwVP1kei9yC1UDWCU5YBit84_5ixXt1q2ZwnS32X5ac8qH
+ngrok authtoken [authtoken]
 
 echo "Starting ngrok to tunnel OpenVPN..."
 ngrok tcp 1194 --region=in --log=stdout &
